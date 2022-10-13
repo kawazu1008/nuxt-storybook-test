c5b4ab1b75a284a77e5db4f48fceb128dc0ba39e
kawazu1008 <kawazu1008@gmail.com>
2022-10-14 00:30:25 +0900

    chore: Storybookのプレビューを調整

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,18 @@
 export * from '~~/.nuxt-storybook/storybook/preview.js'
+
+import Vue from 'vue'
+import '~storybook'
+
+// 対象のコンポーネント
+import Button from '../components/test/Button.vue'
+
+// オブジェクトにまとめる
+const components = {
+  Button: Button
+  // 他のコンポーネントがあればここに追加
+}
+
+// コンポーネントを登録
+Object.keys(components).forEach((name) => {
+  Vue.component(name, components[name])
+})
